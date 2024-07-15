36c5e4812de213c2a511ced96493521ae71d7689
Daniel Hidalgo <daniel.hidalgo@wundertec.com>
2024-07-15 13:27:02 -0600

    nuestros servicios sin responsivo disp moviles

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             contenido.innerHTML = data;
+            elementosInicialesNosotros()
         })
         .catch(error => {
             console.error('Error al cargar el archivo:', error);
@@ -43,6 +44,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 .then(response => response.text())
                 .then(data => {
                     contenido.innerHTML = data;
+                    if(fileName == 'nosotros.html')
+                        elementosInicialesNosotros()
                 })
                 .catch(error => {
                     console.error('Error al cargar el archivo:', error);
@@ -78,6 +81,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 .then(response => response.text())
                 .then(data => {
                     contenido.innerHTML = data;
+                    if(fileName == 'nosotros.html')
+                        elementosInicialesNosotros()
                 })
                 .catch(error => {
                     console.error('Error al cargar el archivo:', error);
@@ -86,3 +91,69 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+function elementosInicialesNosotros() {
+    document.getElementById('img-nube').src = '../assets/img/icono_servicios_nube_white.svg'
+    document.getElementById('img-ciberseguridad').src = '../assets/img/icono_ciberseguridad_white.svg'
+    document.getElementById('img-nearshoring').src = '../assets/img/icono_nearshoring_white.svg'
+}
+
+function showContent(service) {
+    const content = document.getElementById('content-descripcion-servicios');
+    const items = document.querySelectorAll('.menu-item-descripcion-servicios');
+    const backgrounds = document.querySelectorAll('.background-descripcion-servicios');
+    backgrounds.forEach(background => {
+        background.className = 'background-descripcion-servicios';
+    });
+    items.forEach(item => item.classList.remove('active'));
+
+    if (service === 'softlab') {
+        content.innerHTML = `
+            <h2 style="color: #1A9DD9">SoftLab</h2>
+            <div class="subheading">Es nuestro laboratorio de Software a la medida.</div>
+            <p>Vamos más allá que las tradicionales fábricas de software. Nos sumergimos en el corazón de tus necesidades, las entendemos y diseñamos para crear soluciones tecnológicas personalizadas a cada cliente.</p>
+        `;
+        document.querySelector('.menu-item-softlab').classList.add('active');
+        document.querySelector('.background-descripcion-servicios').classList.add('softlab');
+        document.getElementById('img-softlab').src = '../assets/img/icono_softlab.svg'
+        document.getElementById('img-nube').src = '../assets/img/icono_servicios_nube_white.svg'
+        document.getElementById('img-ciberseguridad').src = '../assets/img/icono_ciberseguridad_white.svg'
+        document.getElementById('img-nearshoring').src = '../assets/img/icono_nearshoring_white.svg'
+    } else if (service === 'nube') {
+        content.innerHTML = `
+            <h2 style="color: #7D4593">Servicios de la nube</h2>
+            <div class="subheading">Flexibilidad sin límites.</div>
+            <p>Tenemos experiencia en transformación end-to-end (una solución funcional completa). A través de la nube, migramos, administramos, monitoreamos y creamos soluciones.</p>
+        `;
+        document.querySelector('.menu-item-nube').classList.add('active');
+        document.querySelector('.background-descripcion-servicios').classList.add('nube');
+        document.getElementById('img-softlab').src = '../assets/img/icono_softlab_white.svg'
+        document.getElementById('img-nube').src = '../assets/img/icono_servicios_nube.svg'
+        document.getElementById('img-ciberseguridad').src = '../assets/img/icono_ciberseguridad_white.svg'
+        document.getElementById('img-nearshoring').src = '../assets/img/icono_nearshoring_white.svg'
+    } else if (service === 'ciberseguridad') {
+        content.innerHTML = `
+            <h2 style="color: #21428F">Ciberseguridad</h2>
+            <div class="subheading">Salvaguardamos tu información y sistemas vitales.</div>
+            <p>Aseguramos la continuidad operativa de tu empresa ante riesgos y amenazas como robos, alteraciones, bloqueos y otros ataques que comprometan tus datos y la infraestructura tecnológica.</p>
+        `;
+        document.querySelector('.menu-item-ciberseguridad').classList.add('active');
+        document.querySelector('.background-descripcion-servicios').classList.add('ciberseguridad');
+        document.getElementById('img-softlab').src = '../assets/img/icono_softlab_white.svg'
+        document.getElementById('img-nube').src = '../assets/img/icono_servicios_nube_white.svg'
+        document.getElementById('img-ciberseguridad').src = '../assets/img/icono_ciberseguridad.svg'
+        document.getElementById('img-nearshoring').src = '../assets/img/icono_nearshoring_white.svg'
+    } else if (service === 'nearshoring') {
+        content.innerHTML = `
+            <h2 style="color: #F6A500">Nearshoring</h2>
+            <div class="subheading">Expanda sus capacidades técnicas sin las complicaciones del reclutamiento tradicional.</div>
+            <p>Ofrecemos soluciones de Staff Augmentation integrando talento altamente calificado desde ubicaciones cercanas, garantizando colaboración eficiente, comunicación fluida y alineación cultural.</p>
+        `;
+        document.querySelector('.menu-item-nearshoring').classList.add('active');
+        document.querySelector('.background-descripcion-servicios').classList.add('nearshoring');
+        document.getElementById('img-softlab').src = '../assets/img/icono_softlab_white.svg'
+        document.getElementById('img-nube').src = '../assets/img/icono_servicios_nube_white.svg'
+        document.getElementById('img-ciberseguridad').src = '../assets/img/icono_ciberseguridad_white.svg'
+        document.getElementById('img-nearshoring').src = '../assets/img/icono_nearshoring.svg'
+    }
+}
